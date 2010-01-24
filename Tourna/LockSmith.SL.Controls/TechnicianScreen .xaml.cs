200a074a36c77788783e34dfa9be596636920ca7
piniusha <piniusha@b741058c-8862-11de-ae39-2786408ab335>
2010-01-24 19:00:03 +0000

    1. create silverlight project for locksmith controls

---
+++ Tourna/LockSmith.SL.Controls/TechnicianScreen .xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace LockSmith.SL.Controls
+{
+    public partial class TechnicianScreen : UserControl
+    {
+        public TechnicianScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = this.txtFirstName.Text;
+            string lastName = this.txtFirstName.Text;
+            string phone = this.txtFirstName.Text;
+
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["StrongerOrgString"].ConnectionString))
+            {
+        }
+    }
+}
