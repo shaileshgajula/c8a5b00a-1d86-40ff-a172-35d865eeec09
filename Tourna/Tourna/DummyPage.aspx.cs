f61d5d577b943134636bfe8df46befd454ae0c20
piniusha <piniusha@b741058c-8862-11de-ae39-2786408ab335>
2010-02-10 03:03:57 +0000

    Single elimination algorithm

--- Tourna/Tourna/DummyPage.aspx.cs
+++ Tourna/Tourna/DummyPage.aspx.cs
@@ -13,7 +13,13 @@ namespace StrongerOrg
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            List<Competitor> compList = new List<Competitor>() { new Competitor() { Name = "A" }, new Competitor() { Name = "B" }, new Competitor() { Name = "C" },
+                                                                new Competitor() { Name = "D" }, new Competitor() { Name = "E" }, new Competitor() { Name = "F" }, 
+                                                                new Competitor() { Name = "G" }, new Competitor() { Name = "H" }, new Competitor() { Name = "I" },
+                                                                new Competitor() { Name = "J" }};
+            List<Matchup> mathcupList = this.Execute(compList);
+            this.gvResults.DataSource = mathcupList;
+            this.gvResults.DataBind();
         }
 
         protected void btnCreatPlayers_Click(object sender, EventArgs e)
@@ -23,7 +29,7 @@ namespace StrongerOrg
             for (int i = 0; i < cnt; i++)
             {
                 string name = ((char)(65 + i)).ToString();
-                playerList.Add(new Competitor() { Name = name, Id=i });
+                playerList.Add(new Competitor() { Name = name, Id = i });
             }
             ViewState["PlayerArry"] = playerList;
             this.gvResults.DataSource = playerList;
@@ -69,53 +75,93 @@ namespace StrongerOrg
 
         private void SetBye(List<Competitor> playerList, int byeCnt)
         {
-            for (int i = (playerList.Count - 1); i >= (playerList.Count-byeCnt); i--)
+            for (int i = (playerList.Count - 1); i >= (playerList.Count - byeCnt); i--)
             {
                 playerList[i].IsBye = true;
                 playerList[i].Round = 2;
             }
 
         }
-       
+
 
         protected void btnSinglePair_Click(object sender, EventArgs e)
         {
             List<Competitor> playerList = ViewState["PlayerArry"] as List<Competitor>;
-            List<Matchup> matchups = new List<Matchup>();
-            for (int i = 0; i < playerList.Count; i++)
+            List<Matchup> mathcupList = this.Execute(playerList);
+            this.gvResults.DataSource = mathcupList;
+            this.gvResults.DataBind();
+
+            //List<Matchup> matchups = new List<Matchup>();
+            //for (int i = 0; i < playerList.Count; i++)
+            //{
+            //    Competitor competitor = playerList[i];
+
+            //    if (competitor.IsBye)
+            //    {
+            //        Matchup match = new Matchup();
+            //        match.PlayerA = competitor.Name;
+            //        match.Round = competitor.Round;
+            //        //match.MatchUpId = string.Format("MatchUp{0}", (matchups.Count + 1).ToString());
+            //        matchups.Add(match);
+            //    }
+            //    else
+            //    {
+            //        Matchup match = new Matchup();
+            //        match.PlayerA = competitor.Name;
+            //        match.Round = competitor.Round;
+            //        i++;
+            //        //match.MatchUpId = string.Format("MatchUp{0}", (matchups.Count + 1).ToString());
+            //        competitor = playerList[i];
+            //        match.PlayerB = competitor.Name;
+            //        matchups.Add(match);
+            //    }
+            //}
+            //this.gvPairs.DataSource = matchups;
+            //this.gvPairs.DataBind();
+            //this.Bracket1.DataSource = playerList;
+            //this.Bracket1.DataCompetitorNameField = "Name";
+            //this.Bracket1.DataCompetitorIdField = "Id";
+            //this.Bracket1.Results.Add(new BracketMatchupResult() { MatchupID = "MatchUp1", WinningCompetitorId = "1" });
+            //this.Bracket1.Results.Add(new BracketMatchupResult() { MatchupID = "MatchUp2", WinningCompetitorId = "6" });
+            //this.Bracket1.Results.Add(new BracketMatchupResult() { MatchupID = "MatchUp3", WinningCompetitorId = "0" });
+            //this.Bracket1.DataBind();
+
+        }
+
+        public List<Matchup> Execute(List<Competitor> compList)
+        {
+            int compCount = compList.Count;
+            int numberOfRounds = (int)Math.Ceiling(Math.Log(compCount, 2));
+            List<Matchup> matchupList = new List<Matchup>();
+            int nextMatch = 0;
+            int matchUpCounter = 0;
+            for (int r = 1; r <= numberOfRounds; r++)
             {
-                Competitor competitor = playerList[i];
-                
-                if (competitor.IsBye) 
+                int compInRound = 0;
+                if (r == 1)
                 {
-                    Matchup match = new Matchup();
-                    match.PlayerA = competitor.Name;
-                    match.Round = competitor.Round;
-                    match.MatchUpId = string.Format("MatchUp{0}", (matchups.Count + 1).ToString());
-                    matchups.Add(match);
+                    compInRound = 2 * compCount - (int)Math.Pow(2, numberOfRounds);
                 }
                 else
                 {
-                    Matchup match = new Matchup();
-                    match.PlayerA = competitor.Name;
-                    match.Round = competitor.Round;
-                    i++;
-                    match.MatchUpId = string.Format("MatchUp{0}", (matchups.Count+1).ToString());
-                    competitor = playerList[i];
-                    match.PlayerB = competitor.Name;
-                    matchups.Add(match);
+                    compInRound = (int)Math.Pow(2, numberOfRounds - r) * 2;
                 }
+                nextMatch += compInRound/2;
+                int j = 0;
+                int x = nextMatch;
+
+                for (int i = 0; i < compInRound; i += 2)
+                {
+                    
+                    x += (((j%2) == 0)? 1:0);
+                    matchupList.Add(new Matchup() { MatchUpId = ++matchUpCounter,  Round = r, PlayerA = compList[i].Name, PlayerB = compList[i + 1].Name, NextMatchId = (r == numberOfRounds)?-1:x });
+                    compList.Add(new Competitor());
+                    j++;
+                    
+                }
+                compList.RemoveRange(0, compInRound);
             }
-            this.gvPairs.DataSource = matchups;
-            this.gvPairs.DataBind();
-            this.Bracket1.DataSource = playerList;
-            this.Bracket1.DataCompetitorNameField = "Name";
-            this.Bracket1.DataCompetitorIdField = "Id";
-            this.Bracket1.Results.Add(new BracketMatchupResult() { MatchupID = "MatchUp1", WinningCompetitorId = "1" });
-            this.Bracket1.Results.Add(new BracketMatchupResult() { MatchupID = "MatchUp2", WinningCompetitorId = "6" });
-            this.Bracket1.Results.Add(new BracketMatchupResult() { MatchupID = "MatchUp3", WinningCompetitorId = "0" });
-            this.Bracket1.DataBind();
-            
+            return matchupList;
         }
     }
     [Serializable]
@@ -130,9 +176,10 @@ namespace StrongerOrg
     }
     public class Matchup
     {
-        public string MatchUpId { get; set; }
+        public int MatchUpId { get; set; }
         public int Round { get; set; }
         public string PlayerA { get; set; }
         public string PlayerB { get; set; }
+        public int NextMatchId { get; set; }
     }
 }
