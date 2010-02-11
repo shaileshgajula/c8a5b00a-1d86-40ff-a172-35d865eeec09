51a178ff523401093dde3f164e27c84a3372d999
piniusha <piniusha@b741058c-8862-11de-ae39-2786408ab335>
2010-02-11 01:18:20 +0000

    Locksmith control - silverlight UI + asp.net server communicating SQL server via WCF service

---
+++ trunk/LocksmithControl/Locksmith.Web/IServiceLocksmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Locksmith.Web
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceLocksmith" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceLocksmith
+    {
+        [OperationContract]
+        List<Technician> GetTechnicianList();
+
+        [OperationContract]
+        bool InsertNewTechnician(string firstName, string lastName,
+                                 string address, string state, string city, string company, string email, string phone, string mobilePhone);
+
+        [OperationContract]
+       List<Company> GetCompanyList();
+
+        [OperationContract]
+        bool InsertNewComapny(string name, string address, string state, string city, string zip, string url,
+                                     string email, string phone1, string phone2, string phone3, string fax);
+
+       
+    }
+}
